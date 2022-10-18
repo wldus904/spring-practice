4f466c9d91399a684b0efbe7b9b563b3c7cc75ed
wldus904 <jypark2781@gmail.com>
2022-10-18 17:30:27 +0900

    계사기 수정

--- src/main/java/com/ariel/oop/practice/Calculator.java
+++ src/main/java/com/ariel/oop/practice/Calculator.java
@@ -1,6 +1,15 @@
 package com.ariel.oop.practice;
 
+import java.util.List;
+
+import com.ariel.oop.practice.calculate.AdditionOperator;
+import com.ariel.oop.practice.calculate.DivisionOperator;
+import com.ariel.oop.practice.calculate.MultiplicationOperator;
+import com.ariel.oop.practice.calculate.SubtractionOperator;
+
 public class Calculator {
+	private static final List<NewArithneticOperator> arithneticOperator = List.of(new AdditionOperator(), new SubtractionOperator(), new MultiplicationOperator(), new DivisionOperator());
+	
 	public static int calculate(int operand1,String operator, int operand2) {
 		return ArithneticOperator.calculates(operand1, operator, operand2); // ENUM에 작업 위임
 	}
